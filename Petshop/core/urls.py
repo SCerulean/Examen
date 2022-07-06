6dbe6af44a031a3028cf9874bfb0e57384963b35
SCerulean <zjeremy507@gmail.com>
2022-07-06 15:58:58 -0400

    avance inicio sesion y form

--- Petshop/core/urls.py
+++ Petshop/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, inicio_sesion
 #--from rest_framework import routers
 
 
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path("inicio-sesion",inicio_sesion,name="inicio_sesion")
 ]
--- Petshop/core/views.py
+++ Petshop/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home (request):
     return render(request,'core/inicio_Catalogo.html')
+
+def create_acount(request):
+    return render(request,'core/create_acount_form.html' )
+
+def inicio_sesion(request):
+    return render(request,'core/inicio_sesion.html' )
