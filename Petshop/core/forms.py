e4d8db94c30e9c95f6f0c2b3d944f50554831da3
SCerulean <zjeremy507@gmail.com>
2022-07-11 15:01:11 -0400

    api rest

--- Petshop/core/forms.py
+++ Petshop/core/forms.py
@@ -1,7 +1,26 @@
 from dataclasses import fields
 from pyexpat import model
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Widget
+from .models import Cuenta_cliente
+
+        
+class registro (ModelForm):
+         
+    nombre = forms.CharField(label="Nombre",widget=forms.TextInput(attrs={"placeholder": "Escribe tu nombre"}))
+    correo = forms.CharField(label="Correo",widget=forms.TextInput(attrs={"placeholder": "Escribe tu email"}))
+    contraseña = forms.CharField(label="Contraseña",widget=forms.TextInput(attrs={"placeholder": "Escribe tu contraseña"}))
+    contraseña2 = forms.CharField(label="Repita su Contraseña",widget=forms.TextInput(attrs={"placeholder": ""}))
+    class Meta :
+        model = Cuenta_cliente
+        fields= ['nombre','correo','contraseña','contraseña2',]
 
 
-# Clase ProductosForm --
+class login (ModelForm):
+         
+    nombre = forms.CharField(label="Nombre",widget=forms.TextInput(attrs={"placeholder": "Escribe tu nombre"}))
+    contraseña = forms.CharField(label="Contraseña",widget=forms.TextInput(attrs={"placeholder": "Escribe tu contraseña"}))
+    class Meta :
+        model = Cuenta_cliente
+        fields= ['nombre','contraseña',]
+   
