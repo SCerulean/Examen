b272b0892eb7108fd34ac7d7b6c89bf130c07356
Mishi <michael.encina.1324@gmail.com>
2022-07-11 16:57:21 -0400

    traerdatosacartas

--- Petshop/core/static/core/JS/Carrito.js
+++ Petshop/core/static/core/JS/Carrito.js
@@ -1,3 +1,7 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -6,10 +10,26 @@ const fetchData = async () => {
     try {
         const res = await fetch('https://jsonplaceholder.typicode.com/posts')
         const data = await res.json()
-        console.log(data)
-        
+        //console.log(data)
+        Cartitas(data)
     } catch (error) {
         console.log(error)
         
     }
+}
+
+const Cartitas = data => {
+
+    data.forEach(producto => {
+
+        console.log(producto)
+        templateCard.querySelector('h5').textContent = producto.title
+
+
+        const clone = templateCard.cloneNode (true)
+        fragment.appendChild(clone)
+
+    })
+
+    items.appendChild(fragment)
 }
